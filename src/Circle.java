6317a3e8472296d2187bd31211b59ca972089535
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-27 21:27:40 -0400

    Circle, Circle App, and Validator

---
+++ src/Circle.java
@@ -0,0 +1,41 @@
+
+public class Circle {
+
+	public Circle(double radius) { //radius of a circle *2
+		double circle = 2 * radius;
+		return;
+	}
+
+	public static double getCircumference(int radius) {
+		double circumference = 2 * Math.PI * radius;
+		return circumference;
+	}
+
+	public String getFormattedCircumference() {
+		double circumference = 0.00;
+		return null;
+	}
+
+	public static double getArea(double radius) {
+		double area = Math.PI * (radius * radius);
+		return area;
+	}
+
+	public String getFormattedArea() {
+		double area = 0.00;
+		return null;
+	}
+
+	private String formatNumber(double x) {//why do i need this?
+		return null;
+
+	}
+
+	public static int getObjectCount(String choice, int value) {
+		if (choice.equals("y")) {// counts the number of times user builds circle object
+			value = value + 1;
+		}
+		return value;
+	}
+
+}// end public class Circle
